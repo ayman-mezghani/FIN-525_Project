d192b7d7b915c7640289545e95fd62d150b30c29
Ayman <ayman.mezghani@epfl.ch>
2022-01-28 20:09:24 +0100

    utils for plots

---
+++ utils.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot(df, x, y, hue=None, title='Ticker', x_label=None, y_label=None, save_name=None):
+    ax = sns.lineplot(data=df, x=x, y=y, hue=hue)
+    ax.set_title(title)
+    plt.xticks(rotation=45)
+    
+    if x_label:
+        ax.set_xlabel(x_label)
+        
+    if y_label:
+        ax.set_ylabel(y_label)
+    
+    if save_name:
+        if not os.path.exists('plots'):
+            os.mkdir('plots')
+        plt.savefig(f"plots/{save_name}.svg", bbox_inches = "tight")
+    plt.show()
